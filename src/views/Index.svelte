fbfa73283d85ba38227a06467556ec616f3ce973
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2021-01-18 20:06:46 -0500

    updating changes from hannah

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -30,9 +30,9 @@
           <div class="content">
             <h3>Our Values</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis
-              aute irure dolor in reprehenderit
+              At Thompson Development Group, we strive to Incorporate our values
+              Into our daily practice. We operate our companies based on these
+              values, and hope to share similar values with our customers.
             </p>
           </div>
         </div>
@@ -44,10 +44,10 @@
                 data-aos="fade-up"
                 data-aos-delay="100">
                 <i class="bx bx-receipt" />
-                <h4>Corporis voluptates sit</h4>
+                <h4>Quality Products</h4>
                 <p>
-                  Consequuntur sunt aut quasi enim aliquam quae harum pariatur
-                  laboris nisi ut aliquip
+                  Thompson Development Group is dedicated to creating quality
+                  products to ensure we are providing top-of-the-line services.
                 </p>
               </div>
               <div
@@ -55,10 +55,11 @@
                 data-aos="fade-up"
                 data-aos-delay="200">
                 <i class="bx bx-cube-alt" />
-                <h4>Ullamco laboris nisi</h4>
+                <h4>Attention to Detail</h4>
                 <p>
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                  qui officia deserunt
+                  Thompson Development Group focuses on maintaining a
+                  detail-oriented approach. We like to consider each avenue to
+                  produce the best possible outcome.
                 </p>
               </div>
               <div
@@ -66,10 +67,11 @@
                 data-aos="fade-up"
                 data-aos-delay="300">
                 <i class="bx bx-images" />
-                <h4>Labore consequatur</h4>
+                <h4>Positive Message</h4>
                 <p>
-                  Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut
-                  maiores omnis facere
+                  Thompson Development Group strives to improve the world
+                  arounds us with each an every one of our products. We aim to
+                  spread positivity, hope, and compassion.
                 </p>
               </div>
               <div
@@ -77,10 +79,12 @@
                 data-aos="fade-up"
                 data-aos-delay="400">
                 <i class="bx bx-shield" />
-                <h4>Beatae veritatis</h4>
+                <h4>Commitment to Customers</h4>
                 <p>
-                  Expedita veritatis consequuntur nihil tempore laudantium vitae
-                  denat pacta
+                  Thompson Development Group advocates strongly on behalf of our
+                  customers and strives to ensure quality service. Our
+                  client-focused model reminds us, and our customers, that our
+                  brand is driven by partnership.
                 </p>
               </div>
             </div>
@@ -93,12 +97,12 @@
   <section id="features" class="features" data-aos="fade-up">
     <div class="container">
       <div class="section-title">
-        <h2>Features</h2>
+        <h2>Company Features</h2>
         <p>
-          Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-          aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-          quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-          fugiat sit in iste officiis commodi quidem hic quas.
+          Thompson Development Group prides itself in the way that we provide a
+          variety of services with a variety of services. Ranging from
+          programming to graphic design and marketing to handmade craft creation
+          to digital illustration - we've got it all.
         </p>
       </div>
 
@@ -107,10 +111,7 @@
           <img src="assets/img/features-1.png" class="img-fluid" alt="" />
         </div>
         <div class="col-md-7 pt-4" data-aos="fade-left" data-aos-delay="100">
-          <h3>
-            Voluptatem dignissimos provident quasi corporis voluptates sit
-            assumenda.
-          </h3>
+          <h3>Software Architecture</h3>
           <p class="font-italic">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -137,7 +138,7 @@
           <img src="assets/img/features-2.png" class="img-fluid" alt="" />
         </div>
         <div class="col-md-7 pt-5 order-2 order-md-1" data-aos="fade-right">
-          <h3>Corporis temporibus maiores provident</h3>
+          <h3>Graphic Design & Marketing</h3>
           <p class="font-italic">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -157,10 +158,7 @@
           <img src="assets/img/features-3.png" class="img-fluid" alt="" />
         </div>
         <div class="col-md-7 pt-5" data-aos="fade-left">
-          <h3>
-            Sunt consequatur ad ut est nulla consectetur reiciendis animi
-            voluptas
-          </h3>
+          <h3>Handmade Products</h3>
           <p>
             Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia
             minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor
@@ -188,10 +186,7 @@
           <img src="assets/img/features-4.png" class="img-fluid" alt="" />
         </div>
         <div class="col-md-7 pt-5 order-2 order-md-1" data-aos="fade-right">
-          <h3>
-            Quas et necessitatibus eaque impedit ipsum animi consequatur
-            incidunt in
-          </h3>
+          <h3>Digital Design & Illustration</h3>
           <p class="font-italic">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -216,7 +211,7 @@
           data-aos="fade-up"
           data-aos-delay="100">
           <span>01</span>
-          <h4>Lorem Ipsum</h4>
+          <h4>Errand Apparel</h4>
           <p>
             Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
             consectetur ducimus vero placeat
@@ -228,7 +223,7 @@
           data-aos="fade-up"
           data-aos-delay="200">
           <span>02</span>
-          <h4>Repellat Nihil</h4>
+          <h4>Hap-Bee Canndle Co.</h4>
           <p>
             Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis
             dolor quod et vel dire leno para dest
@@ -240,7 +235,7 @@
           data-aos="fade-up"
           data-aos-delay="300">
           <span>03</span>
-          <h4>Ad ad velit qui</h4>
+          <h4>Sassy Sis Illustrations</h4>
           <p>
             Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt
             debitis quo vel nam quis
@@ -306,7 +301,9 @@
           <div class="icon-box">
             <div class="icon"><i class="bx bxl-dribbble" /></div>
             <h4 class="title"><a href="/">Product Development</a></h4>
-            <p class="description">we create awesome brands</p>
+            <p class="description">
+              We create awesome brands that people love and have fun doing so.
+            </p>
           </div>
         </div>
 
@@ -318,8 +315,9 @@
             <div class="icon"><i class="bx bx-file" /></div>
             <h4 class="title"><a href="/">Testing Professionals</a></h4>
             <p class="description">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore
+              We can provide clients with full-fledged unit tests, and various
+              automated frameworks which allow the company to thrive and provide
+              the ability to put engineers back on development
             </p>
           </div>
         </div>
@@ -332,8 +330,8 @@
             <div class="icon"><i class="bx bx-tachometer" /></div>
             <h4 class="title"><a href="/">Application Development</a></h4>
             <p class="description">
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia
+              We provide the ability to bring your ideas to the client's
+              browsers
             </p>
           </div>
         </div>
@@ -346,8 +344,7 @@
             <div class="icon"><i class="bx bx-layer" /></div>
             <h4 class="title"><a href="/">Graphic Design</a></h4>
             <p class="description">
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              blanditiis
+              We give your ideas a look and feel that everyone will love
             </p>
           </div>
         </div>
@@ -355,110 +352,15 @@
     </div>
   </section>
 
-  <!--<section id="testimonials" class="testimonials section-bg">
-    <div class="container" data-aos="fade-up">
-      <div class="section-title">
-        <h2>Testimonials</h2>
-        <p>
-          Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-          aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-          quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-          fugiat sit in iste officiis commodi quidem hic quas.
-        </p>
-      </div>
-
-      <div class="owl-carousel testimonials-carousel">
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left" />
-            Proin iaculis purus consequat sem cure digni ssim donec porttitora
-            entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam
-            eget nibh et. Maecen aliquam, risus at semper.
-            <i class="bx bxs-quote-alt-right quote-icon-right" />
-          </p>
-          <img
-            src="assets/img/testimonials/testimonials-1.jpg"
-            class="testimonial-img"
-            alt="" />
-          <h3>Saul Goodman</h3>
-          <h4>Ceo &amp; Founder</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left" />
-            Export tempor illum tamen malis malis eram quae irure esse labore
-            quem cillum quid cillum eram malis quorum velit fore eram velit sunt
-            aliqua noster fugiat irure amet legam anim culpa.
-            <i class="bx bxs-quote-alt-right quote-icon-right" />
-          </p>
-          <img
-            src="assets/img/testimonials/testimonials-2.jpg"
-            class="testimonial-img"
-            alt="" />
-          <h3>Sara Wilsson</h3>
-          <h4>Designer</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left" />
-            Enim nisi quem export duis labore cillum quae magna enim sint quorum
-            nulla quem veniam duis minim tempor labore quem eram duis noster
-            aute amet eram fore quis sint minim.
-            <i class="bx bxs-quote-alt-right quote-icon-right" />
-          </p>
-          <img
-            src="assets/img/testimonials/testimonials-3.jpg"
-            class="testimonial-img"
-            alt="" />
-          <h3>Jena Karlis</h3>
-          <h4>Store Owner</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left" />
-            Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos
-            export minim fugiat minim velit minim dolor enim duis veniam ipsum
-            anim magna sunt elit fore quem dolore labore illum veniam.
-            <i class="bx bxs-quote-alt-right quote-icon-right" />
-          </p>
-          <img
-            src="assets/img/testimonials/testimonials-4.jpg"
-            class="testimonial-img"
-            alt="" />
-          <h3>Matt Brandon</h3>
-          <h4>Freelancer</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left" />
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua
-            veniam tempor noster veniam enim culpa labore duis sunt culpa nulla
-            illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-            <i class="bx bxs-quote-alt-right quote-icon-right" />
-          </p>
-          <img
-            src="assets/img/testimonials/testimonials-5.jpg"
-            class="testimonial-img"
-            alt="" />
-          <h3>John Larson</h3>
-          <h4>Entrepreneur</h4>
-        </div>
-      </div>
-    </div>
-  </section>-->
-
   <section id="team" class="team">
     <div class="container" data-aos="fade-up">
       <div class="section-title">
         <h2>Team</h2>
         <p>
-          Our team is two object oriented entrapuerers who are goal driven to
-          deliver the very best products. No matter what comes through our
-          pipeline, it is given detail driven results from conception to reality
+          Our team is made up of two goal-oriented entrepreneurs who are
+          dedicated to deliver the very best products. No matter what challenges
+          arise, we provide detail-driven results, from service conception to
+          production.
         </p>
       </div>
 
@@ -507,119 +409,6 @@
       </div>
     </div>
   </section>
-
-  <!--<section id="faq" class="faq">
-    <div class="container" data-aos="fade-up">
-      <div class="section-title">
-        <h2>Frequently Asked Questions</h2>
-      </div>
-
-      <ul class="faq-list">
-        <li data-aos="fade-up" data-aos-delay="100">
-          <a data-bs-toggle="collapse" class="" data-bs-target="#faq1">Non
-            consectetur a erat nam at lectus urna duis?
-            <i class="icofont-simple-up" /></a>
-          <div id="faq1" class="collapse show" data-bs-parent=".faq-list">
-            <p>
-              Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id
-              volutpat lacus laoreet non curabitur gravida. Venenatis lectus
-              magna fringilla urna porttitor rhoncus dolor purus non.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="200">
-          <a
-            data-bs-toggle="collapse"
-            data-bs-target="#faq2"
-            class="collapsed">Feugiat scelerisque varius morbi enim nunc
-            faucibus a pellentesque?
-            <i class="icofont-simple-up" /></a>
-          <div id="faq2" class="collapse" data-bs-parent=".faq-list">
-            <p>
-              Dolor sit amet consectetur adipiscing elit pellentesque habitant
-              morbi. Id interdum velit laoreet id donec ultrices. Fringilla
-              phasellus faucibus scelerisque eleifend donec pretium. Est
-              pellentesque elit ullamcorper dignissim. Mauris ultrices eros in
-              cursus turpis massa tincidunt dui.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="300">
-          <a
-            data-bs-toggle="collapse"
-            data-bs-target="#faq3"
-            class="collapsed">Dolor sit amet consectetur adipiscing elit
-            pellentesque habitant morbi?
-            <i class="icofont-simple-up" /></a>
-          <div id="faq3" class="collapse" data-bs-parent=".faq-list">
-            <p>
-              Eleifend mi in nulla posuere sollicitudin aliquam ultrices
-              sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla
-              pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque
-              eu tincidunt. Lectus urna duis convallis convallis tellus. Urna
-              molestie at elementum eu facilisis sed odio morbi quis
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="400">
-          <a
-            data-bs-toggle="collapse"
-            data-bs-target="#faq4"
-            class="collapsed">Ac odio tempor orci dapibus. Aliquam eleifend mi
-            in nulla?
-            <i class="icofont-simple-up" /></a>
-          <div id="faq4" class="collapse" data-bs-parent=".faq-list">
-            <p>
-              Dolor sit amet consectetur adipiscing elit pellentesque habitant
-              morbi. Id interdum velit laoreet id donec ultrices. Fringilla
-              phasellus faucibus scelerisque eleifend donec pretium. Est
-              pellentesque elit ullamcorper dignissim. Mauris ultrices eros in
-              cursus turpis massa tincidunt dui.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="500">
-          <a
-            data-bs-toggle="collapse"
-            data-bs-target="#faq5"
-            class="collapsed">Tempus quam pellentesque nec nam aliquam sem et
-            tortor consequat?
-            <i class="icofont-simple-up" /></a>
-          <div id="faq5" class="collapse" data-bs-parent=".faq-list">
-            <p>
-              Molestie a iaculis at erat pellentesque adipiscing commodo.
-              Dignissim suspendisse in est ante in. Nunc vel risus commodo
-              viverra maecenas accumsan. Sit amet nisl suscipit adipiscing
-              bibendum est. Purus gravida quis blandit turpis cursus in
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="600">
-          <a
-            data-bs-toggle="collapse"
-            data-bs-target="#faq6"
-            class="collapsed">Tortor vitae purus faucibus ornare. Varius vel
-            pharetra vel turpis nunc eget lorem dolor?
-            <i class="icofont-simple-up" /></a>
-          <div id="faq6" class="collapse" data-bs-parent=".faq-list">
-            <p>
-              Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris
-              vitae ultricies leo integer malesuada nunc vel. Tincidunt eget
-              nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut
-              venenatis tellus in metus vulputate eu scelerisque. Pellentesque
-              diam volutpat commodo sed egestas egestas fringilla phasellus
-              faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
-            </p>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </section>-->
 
   <section id="contact" class="contact section-bg">
     <div class="container" data-aos="fade-up">
